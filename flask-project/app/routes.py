e8287e07dfcc457ab02a524395ad225a347f49c5
LofiTurtle <46658877+LofiTurtle@users.noreply.github.com>
2022-12-06 12:08:27 -0800

    Remove hard-coded URL for splash page

--- flask-project/app/routes.py
+++ flask-project/app/routes.py
@@ -1,7 +1,8 @@
-from app import myapp_obj,db
+import requests
+from app import myapp_obj, db
 from flask import render_template, redirect, flash
-from app.forms import LoginForm,MessageForm
-from app.models import User,Message
+from app.forms import LoginForm, MessageForm
+from app.models import User, Message
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import current_user
 from flask_login import login_required
@@ -12,7 +13,7 @@ from flask_login import logout_user
 @myapp_obj.route('/')
 def hello():
     if current_user.is_authenticated:
-        messages = Message.query.filter(User.username=='erich').all()
+        messages = Message.query.filter(User.username == 'erich').all()
         return render_template('home.html', messages=messages)
     else:
         # user is not logged in, show default splash page instead
@@ -20,10 +21,10 @@ def hello():
         unsplash_parameters = {'collections': '11649432',
                                'orientation': 'landscape'}
         unsplash_url = f'https://api.unsplash.com/photos/random'
-        # r = requests.get(unsplash_url, params=unsplash_parameters, headers=unsplash_access_key).json()
-        r = {'urls': {
-            'raw': 'https://images.unsplash.com/photo-1543337212-8c58be380d9d?ixid=MnwzODUwNDd8MHwxfHJhbmRvbXx8fHx8fHx8fDE2Njk5MjYxMzA&amp;ixlib=rb-4.0.3'
-        }}
+        r = requests.get(unsplash_url, params=unsplash_parameters, headers=unsplash_access_key).json()
+        # r = {'urls': {
+        #     'raw': 'https://images.unsplash.com/photo-1543337212-8c58be380d9d?ixid=MnwzODUwNDd8MHwxfHJhbmRvbXx8fHx8fHx8fDE2Njk5MjYxMzA&amp;ixlib=rb-4.0.3'
+        # }}
         # print(r)
         # print(r.json())
         return render_template('splash.html', image_url=r['urls']['raw'])
@@ -34,11 +35,13 @@ def like(id):
     # toggle messages[id][liked]
     return redirect('/')
 
+
 @myapp_obj.route('/logout/')
 @login_required
 def logout():
     logout_user()
     return redirect('/')
+
 
 @myapp_obj.route('/login/', methods=['POST', 'GET'])
 def login():
@@ -62,14 +65,13 @@ def login():
         flash('another quick way to debug')
         print(current_form.username.data, current_form.password.data)
         return redirect('/')
-    a = 1
-    name = 'Carlos'
-    return render_template('login.html', name=name, a=a, form=current_form)
+    return render_template('login.html', form=current_form)
 
 
 @myapp_obj.route('/user/<string:username>/')
 def username(username):
     return render_template('username.html', username=username)
+
 
 @myapp_obj.route('/message', methods=['POST', 'GET'])
 def message():
@@ -78,7 +80,7 @@ def message():
 
     current_form = MessageForm()
     if current_form.validate_on_submit():
-        m = Message(contents=current_form.message.data,like_count=0,user_id=current_user.id)
+        m = Message(contents=current_form.message.data, like_count=0, user_id=current_user.id)
         db.session.add(m)
         db.session.commit()
         return redirect('/')
